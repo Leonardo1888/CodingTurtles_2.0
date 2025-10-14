f47e05383b4629a8cf723c90b83254b23b466c8a
Leonardo1888 <leonardorota18@gmail.com>
2025-10-14 19:50:59 +0200

    add: app clienti e abbonamenti

--- abbonamenti/views.py
+++ abbonamenti/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Ciao, sei nella pagina degli abbonamenti.")
+    return render(request, "sale/sale.html")
--- sale/views.py
+++ sale/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def sale(request):
+def index(request):
     return render(request, "sale/sale.html")
