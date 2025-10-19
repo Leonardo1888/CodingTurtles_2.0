2807b7b38de9fa6665bb9e2efe86edfbf7494bd6
Pietro <p.bonomelli1@studenti.unibg.it>
2025-10-19 22:46:58 +0200

    Implementato popolamento tabella pagina Abbonamento

--- abbonamenti/views.py
+++ abbonamenti/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from clienti.models import Abbonamento
 
 def index(request):
-    return render(request, "abbonamenti/abbonamenti.html")
+    abbonamenti = Abbonamento.objects.all().order_by('nAbb')
+    abbonamenti_data = []
+    for abbonamento in abbonamenti:
+        abbonamenti_data.append({
+            "nAbb": abbonamento.nAbb,
+            "cliente": abbonamento.cliente,
+            "inizio": abbonamento.inizio,
+            "fine": abbonamento.fine,
+            "prezzo": abbonamento.prezzo,
+        })
+    return render(request, "abbonamenti/abbonamenti.html", {"abbonamenti_data": abbonamenti_data})
