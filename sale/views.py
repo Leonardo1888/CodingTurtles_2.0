d44017ee20528620423476f1be99ce43037d733d
Pietro <p.bonomelli1@studenti.unibg.it>
2025-10-19 21:57:59 +0200

    Implementato popolamento tabella pagina Sale

--- sale/views.py
+++ sale/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Sala, FasciaOraria, Prenotazione
 
 def index(request):
-    return render(request, "sale/sale.html")
+    sale = Sala.objects.all()
+    sale_data = []
+    for sala in sale:
+        n_fasce = FasciaOraria.objects.filter(sala=sala).count()
+        n_prenotazioni = Prenotazione.objects.filter(sala=sala).count()
+        sale_data.append({
+            "codice": sala.codice,
+            "nome": sala.nome,
+            "tema": sala.tema,
+            "mq": sala.mq,
+            "n_fasce": n_fasce,
+            "n_prenotazioni": n_prenotazioni,
+        })
+    return render(request, "sale/sale.html", {"sale_data": sale_data})
