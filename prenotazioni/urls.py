331895809c42ec9192e8a3273ee6dd5265c76979
Pietro <p.bonomelli1@studenti.unibg.it>
2025-11-04 15:01:29 +0100

    Implementato collegamento Clienti->Prenotazioni e sistemato Sale->Prenotazioni

--- prenotazioni/urls.py
+++ prenotazioni/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+#aggiunto sala/ o cliente/ all'inizio url per distinguere
 urlpatterns = [
-    path("<str:codice_sala>/", views.prenotazioni, name="prenotazioni"),
+    path("sala/<str:codice_sala>/", views.prenotazioni_sala, name="prenotazioni_sala"),    #collegamento con sale
+    path("cliente/<str:codice_cliente>/", views.prenotazioni_cliente, name="prenotazioni_cliente"),    #collegamento con clienti
 ]
