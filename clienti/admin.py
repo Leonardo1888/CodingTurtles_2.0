0880c74abb94abee4f0308f247fe4217e0103204
Leonardo1888 <leonardorota18@gmail.com>
2025-11-13 18:11:52 +0100

    rimosso admins.py non usato

--- clienti/admin.py
+++ clienti/admin.py
@@ -1,25 +1,3 @@
 from django.contrib import admin
-from .models import Cliente, Abbonamento, SubAbbonamento
 
-class ClienteAdmin(admin.ModelAdmin):  
-    list_display = ('codice', 'nome', 'cognome', 'cf', 'dataNas', 'indirizzo', 'tel', 'email')
-    search_fields = ('codice', 'nome', 'cognome', 'cf', 'email')
-    list_filter = ('dataNas',)
-    ordering = ('cognome', 'nome')
-
-class AbbonamentoAdmin(admin.ModelAdmin):
-    list_display = ('nAbb', 'cliente', 'inizio', 'fine', 'prezzo')
-    search_fields = ('nAbb', 'cliente')
-    list_filter = ('inizio', 'fine')
-    ordering = ('-fine',)
-    raw_id_fields = ('cliente',)
-
-class SubAbbonamentoAdmin(admin.ModelAdmin):
-    list_display = ('prenotazione', 'abbonamento')
-    search_fields = ('prenotazione', 'abbonamento')
-    list_filter = ('prenotazione', 'abbonamento')
-    ordering = ('prenotazione',)
-
-admin.site.register(Cliente, ClienteAdmin)
-admin.site.register(Abbonamento, AbbonamentoAdmin)
-admin.site.register(SubAbbonamento, SubAbbonamentoAdmin)
+# Register your models here.
--- sale/admin.py
+++ sale/admin.py
@@ -1,31 +1,3 @@
 from django.contrib import admin
-from .models import Sala, FasciaOraria, Prenotazione, Posto
 
-class SalaAdmin(admin.ModelAdmin):
-    list_display = ('codice', 'nome', 'tema', 'mq')  # Campi da mostrare nella lista
-    search_fields = ('codice', 'nome', 'tema', 'mq')  # Campi su cui effettuare la ricerca
-    list_filter = ('codice', 'nome', 'tema', 'mq')  # Filtri laterali
-    ordering = ('codice',)  # Ordinamento di default
-
-class FasciaOrariaAdmin(admin.ModelAdmin):
-    list_display = ('sala', 'data', 'ora', 'durata')  # Campi da mostrare nella lista
-    search_fields = ('sala', 'data', 'ora', 'durata')  # Campi su cui effettuare la ricerca
-    list_filter = ('sala', 'data', 'ora', 'durata')  # Filtri laterali
-    ordering = ('sala', 'data', 'ora')  # Ordinamento di default
-
-class PrenotazioneAdmin(admin.ModelAdmin):
-    list_display = ('cliente', 'sala', 'data', 'ora', 'posto', 'abbonamento')  # Campi da mostrare nella lista
-    search_fields = ('cliente', 'sala', 'data', 'ora', 'posto', 'abbonamento')  # Campi su cui effettuare la ricerca
-    list_filter = ('cliente', 'sala', 'data', 'ora', 'posto', 'abbonamento')  # Filtri laterali
-    ordering = ('nProg',)  # Ordinamento di default
-
-class PostoAdmin(admin.ModelAdmin):
-    list_display = ('nProg', 'sala', 'data', 'ora')  # Campi da mostrare nella lista
-    search_fields = ('nProg', 'sala', 'data', 'ora')  # Campi su cui effettuare la ricerca
-    list_filter = ('nProg', 'sala', 'data', 'ora')  # Filtri laterali
-    ordering = ('nProg',)  # Ordinamento di default
-
-admin.site.register(Sala, SalaAdmin)
-admin.site.register(FasciaOraria, FasciaOrariaAdmin)
-admin.site.register(Prenotazione, PrenotazioneAdmin)
-admin.site.register(Posto, PostoAdmin)
+# Register your models here.
